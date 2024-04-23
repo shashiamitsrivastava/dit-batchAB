86dfe1c255e9bb32ba9ce62af6e85852a3386c47
shashiamitsrivastava <92148217+shashiamitsrivastava@users.noreply.github.com>
2024-04-23 19:43:49 +0530

    Add files via upload

--- Demo2.java
+++ Demo2.java
@@ -1,11 +1,20 @@
+class A{
+    private int marks[] = new int[3];
+    A(){
+        System.out.println("Default Cons");
+    }
+    A(int x){
+        this(); // default constructor
+        System.out.println("Param Cons");
+    }
+    A(int x, int y){
+        this(x); // call one param constructor
+        System.out.println("2 Param Cons");
+    }
+}
 public class Demo2 {
     public static void main(String[] args) {
-        int x = 10;
-        int y = x;
-        y++;
-        System.out.println(y);
-        System.out.println(y+1);
-        System.out.println(y);
-        System.out.println(x);
-    }
+        A obj = new A(10,20); // calling 2 param constructor
+        //System.out.println("Success");
+    }   
 }
