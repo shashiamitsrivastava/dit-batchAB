1f4980232866099472a58016657a86d5b47d4c66
shashiamitsrivastava <92148217+shashiamitsrivastava@users.noreply.github.com>
2024-06-06 17:47:10 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,6 +1,23 @@
-class Second{
-    public static void main(String args[]){
-        System.out.println("Hello Java");
-       
+class First{
+    static void show(int []a){
+        a[0] = 100;
+        for(int i : a){
+            System.out.println(i);
+        }
     }
+    public static void main(String[] args) {
+        //int arr[] = new int[3]; // all are fill with 0
+        // {0,0,0}
+        //int []arr = new int[3];
+        //int [] arr = {10,20,30};
+        int [] arr = new int[]{10,20,30};
+        show(arr);
+        System.out.println("After Show Call ");
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+
+    }
+
 }
